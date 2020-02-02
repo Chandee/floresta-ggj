a7d96eb4a55cced9758ecf7d6403d7746bd3405c
alexandre <chandemoraes@hotmail.com>
2020-02-02 10:15:40 -0300

    Updates

---
+++ precache-manifest.cc775cb45ed24b027a36dc78b2cc18a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617a52e292505f8648fb6eadd650ff2e",
+    "url": "/Chandee/floresta-ggj/index.html"
+  },
+  {
+    "revision": "f4b0e2a26241e739ee94",
+    "url": "/Chandee/floresta-ggj/static/css/main.7f88a147.chunk.css"
+  },
+  {
+    "revision": "026d1995d9a72b8519a4",
+    "url": "/Chandee/floresta-ggj/static/js/2.01427968.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Chandee/floresta-ggj/static/js/2.01427968.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b0e2a26241e739ee94",
+    "url": "/Chandee/floresta-ggj/static/js/main.155170b8.chunk.js"
+  },
+  {
+    "revision": "b1d01d618dfaa8956928",
+    "url": "/Chandee/floresta-ggj/static/js/runtime-main.8548ca29.js"
+  },
+  {
+    "revision": "30d764d00fed53f53e11d41a3c32fa86",
+    "url": "/Chandee/floresta-ggj/static/media/type_writer.30d764d0.ttf"
+  }
+]);
